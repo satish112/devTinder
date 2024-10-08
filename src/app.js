3d6aa147551d2fc6e46ee5b265e726738c74a082
sv-mbp <sv-mbp@sv-mbps-MacBook-Pro.local>
2024-10-07 20:21:15 -0700

    routing and middleware

--- src/app.js
+++ src/app.js
@@ -3,31 +3,36 @@ const express = require('express');
 const app = express();
 
 
-
-app.use("/user", (req, res)=>{
-    res.send("using multiple scenarios");
+//handle auth middleware for all request GET, POST, 
+app.use("/admin", (req, res, next)=>{
+    const token = "xyzdsdsds";
+    const isAdminauthorized =  token === "xyz";
+    if (!isAdminauthorized){
+        res.status(401).send("unauthorized request")
+    }
+    else{
+        next();
+    }
 })
 
-//this will handle GET call to /user
-app.get("/user", (req, res)=>{
-     
-    res.send({firstName : "satish", lastname: "rella"});
-})
-
-
-app.post("/user", (req, res)=>{
-     res.send("data successfully post to the datbase");
-})
-
-app.delete("/user", (req, res)=>{
-    res.send("data successfully deleted");
-})
-
-
-// this will match all the HTTP method API calls to /test
-app.use("/test", (req,res)=>{
-    res.send("Hello from the server");
-})
-app.listen((7777), ()=>{
-    console.log("successfuly listening to prot 7777.....");
+app.get("/user",(req, res)=>{
+    // logic of checking if the request is authorized
+        res.send("User Data sent");
 });
+
+
+app.get("/admin/getAlldata",(req, res)=>{
+    // logic of checking if the request is authorized
+        res.send("All Data sent");
+});
+
+
+app.get("/admin/deleteUser",(req, res)=>{
+    // Logic of fetching all data
+    res.send("deleted user")
+});
+
+app.listen((7777), ()=>{
+    console.log("successfuly listening to port 7777.....");
+});
+
